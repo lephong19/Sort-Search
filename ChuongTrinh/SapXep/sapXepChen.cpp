613655dd867e036cbf0c0bbcc5cfe5be5a3a835b
Phong Toan Le <lephong.it19@gmail.com>
2026-08-07 14:34:06 +0700

    Sap Xep Chen

--- ChuongTrinh/SapXep/sapXepChen.cpp
+++ ChuongTrinh/SapXep/sapXepChen.cpp
@@ -40,11 +40,11 @@ void xuatDanhSach(list l)
         xuatSinhVien(l.ds[i]) ;
     }
 }
-void pushBackArr(sinhVien *&a,int &n , sinhVien x)  
+void pushBackArr(sinhVien *&a , int &n , sinhVien x) 
 {
-    n=(a==NULL)? 1 : n+1;
-    sinhVien *newArr=new sinhVien[n] ;
-    for(int i=0;i <n-1; i++)
+    n=(a==NULL)?1:n+1;
+    sinhVien *newArr=new sinhVien[n];
+    for(int i=0; i < n-1;i++)
     {
         newArr[i]=a[i];
     }
@@ -54,75 +54,44 @@ void pushBackArr(sinhVien *&a,int &n , sinhVien x)
 }
 void inputFromFile(list &l) 
 {
-    ifstream fin ("sinhVien.txt") ;
+    ifstream fin("sinhVien.txt");
     if(!fin)
     {
-        cout << "Khong mo duoc file";
+        cout << "Khong Mo Duoc File";
         return;
     }
-    sinhVien x ;
-    while(fin >> x.mssv)
-    {
-        fin.ignore() ;
-        getline(fin,x.hoTen,'#');
+    sinhVien x;
+    while(fin >> x.mssv){
+        fin.ignore();
+        getline(fin,x.hoTen ,'#');
         fin.getline(x.ngaySinh,11,'#');
         fin.getline(x.queQuan,100,'#');
         fin >> x.gpa;
-        pushBackArr(l.ds,l.n,x);
-
+        pushBackArr(l.ds,l.n,x) ;
     }
     fin.close() ;
 }
-void sort(list &l) 
+void sapXepChen(list &l)
 {
-    for(int i=0; i < l.n-1; i++) 
+    for(int i=1; i < l.n; i++) 
     {
-        int min_pos=i;
-        for(int j=i+1; j < l.n; j++) 
+        sinhVien key=l.ds[i];
+        int pos=i-1;
+        while(pos >= 0 && l.ds[pos].gpa>key.gpa)
         {
-            if(l.ds[min_pos].hoTen>l.ds[j].hoTen)
-            min_pos=j;
+            l.ds[pos+1]=l.ds[pos];
+            pos--;
         }
-        swap(l.ds[i],l.ds[min_pos]) ;
+        l.ds[pos+1]=key;
     }
-}
-int timKiemNhiPhan(list l, const string &name)
-{
-    int le=0;
-    int ri=l.n-1;
-    while(le <= ri) 
-    {
-        int m=(le+ri)/2;
-        if(l.ds[m].hoTen==name)
-        {
-            return m;
-        }else if(l.ds[m].hoTen>name)
-        {
-            ri=m-1;
-        }else{
-            le=m+1;
-        }
-    }
-    return -1;
 }
 
 int main() 
 {
     list l;
-    init(l);
+    init(l) ;
     inputFromFile(l) ;
+    sapXepChen(l) ;
     xuatDanhSach(l) ;
-    sort(l);
-
-    cout << endl ;
-    cout << "TIM KIEM NHI PHAN" << endl;
-    int pos=timKiemNhiPhan(l,"Nguyen Van An");
-    if(pos!=-1)
-    {
-        cout << "TIM THAY" << endl ;
-        xuatSinhVien(l.ds[pos]);
-    }else{
-        cout << "Khong Tim Thay";
-    }
     return 0;
 }
